1fd952de0090780f22646d9ad99f2dec5f72283b
Eric Rodriguez <eric.rodriguez@eniro.com>
2020-04-16 13:25:48 +0200

    Using Cats IO only

--- src/main/scala/es/hybride/Main.scala
+++ src/main/scala/es/hybride/Main.scala
@@ -1,13 +1,15 @@
 package es.hybride
 
+import cats.implicits._
 import scala.io.Source
+import cats.effect.{ExitCode, IO, IOApp}
 
-object Main {
+object Main extends { //IOApp {
 
   type Key = String
   type DeviceType = String
 
-  private def dataExtractor(line: String): (Key, Option[DeviceType]) = {
+  private def dataExtractor(line: String): IO[(Key, Option[DeviceType])] = IO {
     val keyIdx = line.indexOf("id") + 5
     val deviceTypeIdx = line.indexOf("\"devicetype\":") + 15
     val deviceTypeEndIdx = line.indexOf("\"", deviceTypeIdx)
@@ -18,11 +20,23 @@ object Main {
     (key, deviceType)
   }
 
+  /*private def processList(lines: List[String]): IO[List[(Key, Option[DeviceType])]] =
+    IO(lines.map(dataExtractor))*/
+
+
   private def readFile(fileName: String) = //: List[(Key, Option[DeviceType])] =
-    Source.fromFile(fileName).getLines().map(dataExtractor).toList
+    IO(Source.fromFile(fileName).getLines().toList)
 
   def main(args: Array[String]): Unit = {
-    readFile("just5k.json").take(5).map(println)
+    val program = for {
+      iol <- readFile("just5k.json")
+      ext  <- iol.traverse(s => dataExtractor(s))
+    } yield {
+      ext.take(5).map(println)
+    }
+
+    program.unsafeRunSync()
   }
+
 
 }

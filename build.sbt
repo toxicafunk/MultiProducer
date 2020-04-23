16b81f7cdd080259c8f8e4dbc7538274ba6a65b2
Eric Rodriguez <eric.rodriguez@eniro.com>
2020-04-23 15:07:49 +0200

    Akka Streams

--- build.sbt
+++ build.sbt
@@ -5,11 +5,7 @@ version := "0.1"
 scalaVersion := "2.12.11"
 
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats-effect" % "1.3.0" withSources() withJavadoc(),
-  // available for 2.12, 2.13
-  "co.fs2" %% "fs2-core" % "2.2.1", // For cats 2 and cats-effect 2
-  // optional I/O library
-  "co.fs2" %% "fs2-io" % "2.2.1"
+  "com.typesafe.akka" %% "akka-stream" % "2.6.4"
 )
 
 scalacOptions ++= Seq(
--- src/main/scala/es/hybride/Main.scala
+++ src/main/scala/es/hybride/Main.scala
@@ -1,54 +1,75 @@
 package es.hybride
 
-import cats.implicits._
-import scala.io.Source
-import cats.effect.{ExitCode, IO, IOApp}
+import java.nio.ByteBuffer
+import java.nio.file.{FileSystem, FileSystems}
+import java.util.concurrent.Executors
 
-object Main extends IOApp {
+import akka.actor.ActorSystem
+import akka.serialization.ByteBufferSerializer
 
-  type Key = String
-  type DeviceType = String
+import akka.util.ByteString
+import akka.{Done, NotUsed}
+import com.typesafe.config.Config
 
-  def dataExtractor(line: String): IO[(Key, Option[DeviceType])] = IO {
-    val keyIdx = line.indexOf("id") + 5
-    val deviceTypeIdx = line.indexOf("\"devicetype\":")
-    val key = line.substring(keyIdx, keyIdx + 40)
-    val deviceType: Option[String] =
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
+import scala.util.{Failure, Success}
+
+import akka.stream._
+import akka.stream.scaladsl._
+
+object Main {
+
+  type Key = ByteString
+  type DeviceType = ByteString
+
+  def dataExtractor(line: ByteString): (Key, Option[DeviceType]) = {
+    val keyIdx = line.indexOfSlice("id".getBytes()) + 5
+    val deviceTypeIdx = line.indexOfSlice("\"devicetype\":".getBytes())
+    val key = line.slice(keyIdx, keyIdx + 40)
+    val deviceType =
       if (deviceTypeIdx == -1) None
       else {
-        val deviceTypeEndIdx = line.indexOf("\"", deviceTypeIdx + 15)
-        Some(line.substring(deviceTypeIdx + 15, deviceTypeEndIdx))
+        val deviceTypeEndIdx = line.indexOfSlice("\"".getBytes(), deviceTypeIdx + 15)
+        Some(line.slice(deviceTypeIdx + 15, deviceTypeEndIdx))
       }
 
     (key, deviceType)
   }
 
-  /*private def processList(lines: List[String]): IO[List[(Key, Option[DeviceType])]] =
-    IO(lines.map(dataExtractor))*/
+  val ec: ExecutionContextExecutor =
+    ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
+  val system: ActorSystem = ActorSystem.create()
+  implicit val mat = Materializer(system)
 
-  private def readFile(fileName: String): IO[List[String]] =
-    IO(Source.fromFile(fileName).getLines().toList)
+  val fs: FileSystem = FileSystems.getDefault
+  val source: String => Source[ByteString, Future[IOResult]] = (file: String) =>
+    FileIO.fromPath(fs.getPath(file))
+  val lines: Flow[ByteString, ByteString, NotUsed] = Framing.delimiter(
+    ByteString(System.lineSeparator),
+    10000,
+    allowTruncation = true
+  )
 
-  /*def main(args: Array[String]): Unit = {
-    val program = for {
-      iol <- readFile("just5k.json")
-      ext  <- iol.traverse(s => dataExtractor(s))
-    } yield {
-      ext.take(5).map(println)
-    }
+  val program: String => Future[Done] = (filename: String) =>
+    source(filename)
+      .via(lines)
+      .map(line => dataExtractor(line))
+      .runWith(
+        Sink.foreach(l =>
+          println(s"${l._1.utf8String} -> ${l._2.fold("")(_.utf8String)}")
+        )
+      )
 
-    program.unsafeRunSync()
-  }*/
-
-  override def run(args: List[String]): IO[ExitCode] = {
-    val program = for {
-      iol <- readFile("just5k.json")
-      ext <- iol.traverse(s => dataExtractor(s))
-    } yield {
-      ext.map(println)
-      //ext.take(5).map(println)
-    }
-
-    program.as(ExitCode.Success)
+  def main(args: Array[String]): Unit = {
+    //val filename = args(0)
+    val filename = "just5k.json"
+    val t0 = System.currentTimeMillis()
+    program(filename)
+      .andThen {
+        case Failure(t) => println(t)
+        case Success(v) =>
+          val t1 = System.currentTimeMillis()
+          println("Elapsed time: " + (t1 - t0) + "ms")
+      }(ExecutionContext.global)
   }
 }
